37aa8a6731a02f78af3fe729c1a60fa321a23838
Anton Zarichny <antonzarichny@gmail.com>
2023-08-26 13:20:21 +0300

    Initial Commit

---
+++ AvitoTrainee2023/MainPage/MainPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainPageViewController.swift
+//  AvitoTrainee2023
+//
+//  Created by Антон Заричный on 26.08.2023.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
